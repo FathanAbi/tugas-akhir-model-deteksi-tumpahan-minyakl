b00f9491ea2f1bc755a3cdd5d067b22f5dc2e081
Fathan Abi <abikarami60@gmail.com>
2025-06-11 22:32:57 +0700

    input data

---
+++ simsiam/simsiam/parseJson.py
@@ -0,0 +1,12 @@
+import json
+
+# Load the JSON file
+with open(r'C:\Users\Asus TUF\Documents\code\TA\simsiam\simsiam\TESTING\RF\performance\RF 20250611_120842_results.json', 'r') as f:
+    data = json.load(f)
+
+# Extract the performance dictionary
+performance = data.get('performance', {})
+
+for metric, value in performance.items():
+    formatted_value = f"{value:.6f}".replace('.', ',')
+    print(f"{formatted_value}")
