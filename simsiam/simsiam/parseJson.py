972f9aeafb850a8dea56ba0a7a3fec2425742f87
Fathan Abi <abikarami60@gmail.com>
2025-06-15 23:41:05 +0700

    ipdate sheet

--- simsiam/simsiam/parseJson.py
+++ simsiam/simsiam/parseJson.py
@@ -1,7 +1,7 @@
 import json
 
 # Load the JSON file
-with open(r'C:\Users\Asus TUF\Documents\code\TA\simsiam\simsiam\TESTING\MyMethod\performance\MyMethod 20250615_134218_results.json', 'r') as f:
+with open(r'C:\Users\Asus TUF\Documents\code\TA\simsiam\simsiam\TESTING\new_mymethod\performance\MyMethod 20250615_222906_results.json', 'r') as f:
     data = json.load(f)
 
 # Extract the performance dictionary
