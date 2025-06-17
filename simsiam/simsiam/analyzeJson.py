c4078a666e25199d900c08a5bd189d7c662f6254
Fathan Abi <abikarami60@gmail.com>
2025-06-17 22:47:12 +0700

    testing

--- simsiam/simsiam/analyzeJson.py
+++ simsiam/simsiam/analyzeJson.py
@@ -1,12 +1,50 @@
 import json
 
 # Load the JSON file
-with open(r'C:\Users\Asus TUF\Documents\code\TA\simsiam\simsiam\TESTING\MyMethod\performance\MyMethod 20250611_120532_results.json', 'r') as f:
+with open(r'C:\Users\Asus TUF\Documents\code\TA\simsiam\simsiam\TESTING\SVM\performance\SVM 20250617_164625_results.json', 'r') as f:
     data = json.load(f)
 
-# Extract the performance dictionary
-performance = data.get('prediction')
+predictions = data.get('prediction')
+total_dataset = data.get("performance")
 
+print("=====AUC=====")
 for i in range(10):
-    acc = (performance[i]['correct_total']/performance[i]['total'])
-    print(f"{acc:.4f}")
+    pred = predictions[i]
+    print(f"{pred['AUC']:.4f}".replace('.', ','))
+
+print(f"{total_dataset['AUC']:.4f}".replace('.', ','))
+
+print("=====precision=====")
+for i in range(10):
+    pred = predictions[i]
+    print(f"{pred['precision']:.4f}".replace('.', ','))
+
+print(f"{total_dataset['precision']:.4f}".replace('.', ','))
+
+print("=====recall=====")
+for i in range(10):
+    pred = predictions[i]
+    print(f"{pred['recall']:.4f}".replace('.', ','))
+
+print(f"{total_dataset['recall']:.4f}".replace('.', ','))
+
+print("=====F1 Score=====")
+for i in range(10):
+    pred = predictions[i]
+    print(f"{pred['F1 Score']:.4f}".replace('.', ','))
+
+print(f"{total_dataset['F1 Score']:.4f}".replace('.', ','))
+
+print("=====OA=====")
+for i in range(10):
+    pred = predictions[i]
+    print(f"{pred['OA']:.4f}".replace('.', ','))
+
+print(f"{total_dataset['OA']:.4f}".replace('.', ','))
+
+print("=====AA=====")
+for i in range(10):
+    pred = predictions[i]
+    print(f"{pred['AA']:.4f}".replace('.', ','))
+
+print(f"{total_dataset['AA']:.4f}".replace('.', ','))
